e8e9ea56de1b7f4fc17a64663ed83b168a55643e
iMin <syahjudinazra@gmail.com>
2023-02-25 14:22:57 +0700

    jwtspatie

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,27 +19,38 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create(array_merge([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]));
+        DB::beginTransaction();
 
-        $user = User::create(array_merge([
-            'name' => 'user',
-            'email' => 'user@gmail.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]));
+        try {
+            $admin = User::create(array_merge([
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]));
 
-        $role_admin = Role::create(['name' => 'admin']);
-        $role_user = Role::create(['name' => 'user']);
+            $user = User::create(array_merge([
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]));
 
-        $permission = Permission::create(['name' => 'create role']);
-        $permission = Permission::create(['name' => 'read role']);
-        $permission = Permission::create(['name' => 'update role']);
-        $permission = Permission::create(['name' => 'delete role']);
+            $role_admin = Role::create(['name' => 'admin']);
+            $role_user = Role::create(['name' => 'user']);
 
-        $user->assignRole('admin');
-        $user->assignRole('user');
+            $permission = Permission::create(['name' => 'create role']);
+            $permission = Permission::create(['name' => 'read role']);
+            $permission = Permission::create(['name' => 'update role']);
+            $permission = Permission::create(['name' => 'delete role']);
+
+            $admin->assignRole('admin');
+            $user->assignRole('user');
+
+            DB::commit();
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+        }
+
+
     }
 }
