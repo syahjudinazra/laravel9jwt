92e4eb59bdacf2eebf6b11d0d1cad315c9c1a033
iMin <syahjudinazra@gmail.com>
2023-02-25 11:36:42 +0700

    spatie

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create(array_merge([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]));
+
+        $user = User::create(array_merge([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]));
+
+        $role_admin = Role::create(['name' => 'admin']);
+        $role_user = Role::create(['name' => 'user']);
+
+        $permission = Permission::create(['name' => 'create role']);
+        $permission = Permission::create(['name' => 'read role']);
+        $permission = Permission::create(['name' => 'update role']);
+        $permission = Permission::create(['name' => 'delete role']);
+
+        $user->assignRole('admin');
+        $user->assignRole('user');
+    }
+}
